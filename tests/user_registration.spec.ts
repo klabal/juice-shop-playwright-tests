091ab98ddb48b841730b44ca391e18be06805b76
klabal <klaudijabalcaityte@gmail.com>
2025-10-25 19:02:19 +0300

    updated user registration and github workflows files

--- tests/user_registration.spec.ts
+++ tests/user_registration.spec.ts
@@ -1,23 +1,37 @@
 import { test, expect } from '@playwright/test';
 
 test('Login functionality open site', async ({ page }) => {
-    await page.goto('http://localhost:3000/');  
+    await page.goto('http://localhost:3000/'); 
+    //check title 
     //await page.pause();
 });
-    test('should display registration form', async ({ page }) => {
-    await page.goto('http://localhost:3000/');  
-    await page.pause();
-    await page.getByRole('button', { name: 'Close Welcome Banner' });
-    await page.goto('http://localhost:3000/#/login');
-    //close the welcome banner. click on 'account' at the top
-    //click on 'login', click on 'not yet a customer?'
-    //fill in user registration. enter email. enter password two times.
-    //choose security question from the dropdown
-    //fill in answer. click register
-    //login to account 
-    await page.goto('http://localhost:3000/#/register');
-   // const registrationForm = await page.locator('form#register');
-    //await expect(registrationForm).toBeVisible();
+    test('should display registration form and submit user registration information', async ({ page }) => {
+      await page.goto('http://localhost:3000/');
+      await page.pause();
+      await page.getByRole('button', { name: 'Close Welcome Banner' }).click();
+      await page.pause();
+      await page.getByRole('button', { name: 'Show/hide account menu' }).click();
+      await page.getByRole('menuitem', { name: 'Go to login page' }).click();
+      await page.goto('http://localhost:3000/#/login');
+      await page.getByRole('link', { name: 'Not yet a customer?' }).click();
+      //await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+      await page.goto('http://localhost:3000/#/register');
+      await page.getByRole('textbox', { name: 'Email address field' }).click();
+      await page.getByRole('textbox', { name: 'Email address field' }).fill('test2@example.com');
+      await page.getByRole('textbox', { name: 'Email address field' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field for the password' }).fill('admin123');
+      await page.getByRole('textbox', { name: 'Field for the password' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).fill('admin123');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).press('Tab');
+      await page.getByRole('switch', { name: 'Show password advice' }).press('Tab');
+      await page.locator('.mat-mdc-text-field-wrapper.mdc-text-field.mdc-text-field--outlined.mdc-text-field--focused > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await expect(page.getByRole('listbox', { name: 'Selection list for the' })).toBeVisible();
+
+      await page.getByText('Your eldest siblings middle').click();
+      await page.locator('.mat-mdc-form-field.mat-mdc-form-field-type-mat-input.mat-form-field-appearance-outline.mat-form-field-hide-placeholder > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await page.getByRole('textbox', { name: 'Field for the answer to the' }).fill('none');
+      await page.getByRole('button', { name: 'Button to complete the' }).click();
+      await page.click('button[type="submit"]');
   });
   //  test('should register with valid details', async ({ page }) => {
   //  await page.goto('http://localhost:3000/');  
@@ -28,25 +42,64 @@ test('Login functionality open site', async ({ page }) => {
    // await page.click('button[type="submit"]');
    // await expect(page).toHaveURL('http://localhost:3000/dashboard');
  // });
-    //test('should show error with existing email', async ({ page }) => {
-   // await page.goto('http://localhost:3000/');  
-   // await page.pause();
-   // await page.fill('input[name="email"]', 'user@example.com');
-   // await page.fill('input[name="password"]', 'password');
- //   await page.fill('input[name="confirmPassword"]', 'password');
-  //  await page.click('button[type="submit"]');
-  //  const errorMessage = await page.locator('text=Email already exists');
-    //await expect(errorMessage).toBeVisible();
- //});
+    test('should show error with existing email', async ({ page }) => {
+      await page.goto('http://localhost:3000/');
+      await page.pause();
+      await page.getByRole('button', { name: 'Close Welcome Banner' }).click();
+      await page.pause();
+      await page.getByRole('button', { name: 'Show/hide account menu' }).click();
+      await page.getByRole('menuitem', { name: 'Go to login page' }).click();
+      await page.goto('http://localhost:3000/#/login');
+      await page.getByRole('link', { name: 'Not yet a customer?' }).click();
+      //await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+      await page.goto('http://localhost:3000/#/register');
+      await page.getByRole('textbox', { name: 'Email address field' }).click();
+      await page.getByRole('textbox', { name: 'Email address field' }).fill('test2@example.com');
+      await page.getByRole('textbox', { name: 'Email address field' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field for the password' }).fill('admin123');
+      await page.getByRole('textbox', { name: 'Field for the password' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).fill('admin123');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).press('Tab');
+      await page.getByRole('switch', { name: 'Show password advice' }).press('Tab');
+      await page.locator('.mat-mdc-text-field-wrapper.mdc-text-field.mdc-text-field--outlined.mdc-text-field--focused > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await expect(page.getByRole('listbox', { name: 'Selection list for the' })).toBeVisible();
+
+      await page.getByText('Your eldest siblings middle').click();
+      await page.locator('.mat-mdc-form-field.mat-mdc-form-field-type-mat-input.mat-form-field-appearance-outline.mat-form-field-hide-placeholder > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await page.getByRole('textbox', { name: 'Field for the answer to the' }).fill('none');
+      await page.getByRole('button', { name: 'Button to complete the' }).click();
+      await page.click('button[type="submit"]');
+      const errorMessage = await page.locator('text=Email must be unique');
+    
+ });
     test('should show error with mismatched passwords', async ({ page }) => {
-    await page.goto('http://localhost:3000/');  
-    await page.pause();
-    await page.fill('input[name="email"]', 'user@example.com');
-    await page.fill('input[name="password"]', 'password');
-    await page.fill('input[name="confirmPassword"]', 'differentpassword');
-    await page.click('button[type="submit"]');
-    const errorMessage = await page.locator('text=Passwords do not match');
-    //await expect(errorMessage).toBeVisible();
+      await page.goto('http://localhost:3000/');
+      await page.pause();
+      await page.getByRole('button', { name: 'Close Welcome Banner' }).click();
+      await page.pause();
+      await page.getByRole('button', { name: 'Show/hide account menu' }).click();
+      await page.getByRole('menuitem', { name: 'Go to login page' }).click();
+      await page.goto('http://localhost:3000/#/login');
+      await page.getByRole('link', { name: 'Not yet a customer?' }).click();
+      //await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+      await page.goto('http://localhost:3000/#/register');
+      await page.getByRole('textbox', { name: 'Email address field' }).click();
+      await page.getByRole('textbox', { name: 'Email address field' }).fill('user1@example.com');
+      await page.getByRole('textbox', { name: 'Email address field' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field for the password' }).fill('password');
+      await page.getByRole('textbox', { name: 'Field for the password' }).press('Tab');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).fill('differentpassword');
+      await page.getByRole('textbox', { name: 'Field to confirm the password' }).press('Tab');
+      await page.getByRole('switch', { name: 'Show password advice' }).press('Tab');
+      await page.locator('.mat-mdc-text-field-wrapper.mdc-text-field.mdc-text-field--outlined.mdc-text-field--focused > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await expect(page.getByRole('listbox', { name: 'Selection list for the' })).toBeVisible();
+      await page.getByText('Your eldest siblings middle').click();
+      await page.locator('.mat-mdc-form-field.mat-mdc-form-field-type-mat-input.mat-form-field-appearance-outline.mat-form-field-hide-placeholder > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+      await page.getByRole('textbox', { name: 'Field for the answer to the' }).fill('none');
+      await page.getByRole('button', { name: 'Button to complete the' }).click();
+      await page.click('button[type="submit"]');
+      const errorMessage = await page.locator('text=Passwords do not match');
+      //await expect(errorMessage).toBeVisible();
   });
     test('should redirect to login page', async ({ page }) => {
     await page.goto('http://localhost:3000/');  
